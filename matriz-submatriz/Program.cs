9a90c82bdcfe688d5c6c975881b87b6ee898cd57
Liliane Souza <liliane.a.cruz.souza@gmail.com>
2023-07-02 19:20:06 -0300

    Commit Final

--- matriz-submatriz/Program.cs
+++ matriz-submatriz/Program.cs
@@ -4,90 +4,59 @@ namespace matriz_submatriz
 {
     internal class Program
     {
-        const int TAM_MATRIZ = 4;
-        public static void Resultado(StringBuilder StrB, string Nome, int[,] Matriz)
+        const int LIN_MATRIZ = 4;
+        const int COL_MATRIZ = 3;
+        const int LIN_SUBMATRIZ = 3;
+        const int COL_SUBMATRIZ = 2;
+
+        public static int Resposta(int[,] MatrizPrincipal, int[,] SubMatriz)
         {
-            StrB.Append(Nome + Environment.NewLine);
-            for (int i = 0; i < TAM_MATRIZ; i++)
+            int[] VetOcorrencias = new int[LIN_SUBMATRIZ * COL_SUBMATRIZ];
+            int Indice = 0;
+
+            for (int i = 0; i < LIN_SUBMATRIZ; i++)
             {
-                for (int j = 0; j < TAM_MATRIZ; j++)
+                for (int j = 0; j < COL_SUBMATRIZ; j++)
                 {
-                    StrB.Append(" " + Matriz[i, j].ToString() + " ");
-                }
-                StrB.Append(Environment.NewLine);
-            }
-        }
-
-        public static int[,] MatrizResposta(int[,] MatrizPrincipal)
-        {
-            int x = TAM_MATRIZ, y = TAM_MATRIZ;
-            int[,] MatrizResultante = new int[TAM_MATRIZ, TAM_MATRIZ]
-            {
-                { 0, 0, 0, 0 },
-                { 0, 0, 0, 0 },
-                { 0, 0, 0, 0 },
-                { 0, 0, 0, 0 }
-            };
-
-            object o;
-
-            o = MatrizPrincipal.Clone();
-            MatrizResultante = (int[,])o;
-
-            // Inverter todas as diagonais
-            //Diagonal secundária - DS
-            // (0,3) (1,2) (2,1) (3,0)
-            int j = TAM_MATRIZ - 1;
-            for (int i = 0; i < TAM_MATRIZ; i++)
-            {
-                MatrizResultante[j, i] = MatrizPrincipal[i, j];
-                j--;
-            }
-
-            //Diagonal principal - DP
-            for (int i = 0; i < TAM_MATRIZ; i++)
-            {
-                for (j = 0; j < TAM_MATRIZ; j++)
-                {
-                    if (i == j)
+                    for (int x = 0; x < LIN_MATRIZ; x++)
                     {
-                        MatrizResultante[--x, --y] = MatrizPrincipal[i, j];
+                        for (int y = 0; y < COL_MATRIZ; y++)
+                        {
+                            if (SubMatriz[i, j] == MatrizPrincipal[x, y])
+                                VetOcorrencias[Indice] += 1;
+                        }
                     }
+                    Indice++;
                 }
-
             }
 
-            return MatrizResultante;
+            List<int> QtdOcorrencias = VetOcorrencias.ToList();
+            QtdOcorrencias.Sort();
+
+            return QtdOcorrencias.FirstOrDefault();            
         }
 
         static void Main(string[] args)
         {
-            /*
-             * Exemplo
-             * [1, 2, 3, 4]
-             * [4, 3, 2, 1]
-             * [4, 2, 1, 3]
-             * [3, 1, 2, 4]
-             */
-            int[,] MatrizPrincipal = new int[TAM_MATRIZ, TAM_MATRIZ]
+            int[,] MatrizPrincipal = new int[LIN_MATRIZ, COL_MATRIZ]
             {
-                { 1, 2, 3, 4 },
-                { 4, 3, 2, 1 },
-                { 4, 2, 1, 3 },
-                { 3, 1, 2, 4 }
-            };
-            int[,] SubMatriz = new int[TAM_MATRIZ-1, TAM_MATRIZ-1]
-           {
                 { 1, 2, 3 },
                 { 4, 3, 2 },
-                { 4, 2, 1 }
+                { 4, 2, 1 },
+                { 3, 1, 2 }
+            };
+
+            int[,] SubMatriz = new int[LIN_SUBMATRIZ, COL_SUBMATRIZ]
+           {
+                { 1, 2 },
+                { 4, 3 },
+                { 4, 2 }
            };
 
 
-
-
-
-            Console.WriteLine();
+            int TotalOcorrencias = Resposta(MatrizPrincipal, SubMatriz);
+            
+            Console.WriteLine(TotalOcorrencias);
         }
     }
 }
